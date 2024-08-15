681943543e65e267507201ea652cc151482a5d0e
Larpoux <larpoux@gmail.com>
2024-08-15 18:47:42 +0200

    It compiles with Flutter Sound examples

---
+++ ios/Classes/TaudioPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface TaudioPlugin : NSObject<FlutterPlugin>
+@end
---
+++ lib/taudio.dart
@@ -0,0 +1,11 @@
+export 'src/fs/flutter_sound.dart';
+
+
+
+import 'taudio_platform_interface.dart';
+
+class Taudio {
+  Future<String?> getPlatformVersion() {
+    return TaudioPlatform.instance.getPlatformVersion();
+  }
+}
